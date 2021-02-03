111695b431949a1cba22265e1babd246313c1884
ExhoAR22 <exhoar@gmail.com>
2021-02-03 22:33:49 +0200

    Generation of binary configuration works

--- esp/generate_image.py
+++ esp/generate_image.py
@@ -1,17 +1,23 @@
 from argparse import ArgumentParser
 
+from image_generator import generate
+
 
 def build_argument_parser():
+    """
+    Builds the ArgumentParser object to use when parsing the arguments
+    """
     parser = ArgumentParser()
     parser.add_argument('-c', '--config', help='configuration file name', default='rhapsody.toml')
-    parser.add_argument('-o', '--output', help='output file name', default='esp/esp.img')
+    parser.add_argument('-s', '--size', help='size of the generated image, in MiBs', type=int, default=1)
+    parser.add_argument('-o', '--output', help='output file name', default='esp.img')
     return parser
 
 
 def main():
     parser = build_argument_parser()
     args = parser.parse_args()
-    print(args)
+    generate(args.config, args.size, args.output)
 
 
 if __name__ == '__main__':
--- esp/image_generator/generator.py
+++ esp/image_generator/generator.py
@@ -1,2 +1,13 @@
-def generate(configuration_file, output):
-    pass
+from .config import build_config
+
+
+def generate(config_file, size, output):
+    """
+    Generates an ESP image
+    :param config_file: the name of the configuration file to use
+    :param size: the size of the image, in MiBs
+    :param output: the name of the generated image file
+    """
+    config = build_config(config_file)
+    with open(output, 'wb') as output_file:
+        pass
