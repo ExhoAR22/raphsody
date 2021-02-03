52ce5ebebb2524b803943d4cd0bf620cfe57e201
ExhoAR22 <exhoar@gmail.com>
2021-02-03 20:52:47 +0200

    Added uefi-rs submodule

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::ffi::c_void;
 
 #[no_mangle]
-pub extern "C" fn efi_main(_image_handle: *const u8, _system_table: *const u8) -> u64 {
+pub extern "C" fn efi_main(_image_handle: *const c_void, _system_table: *const c_void) -> u64 {
     loop {}
 }
 
-/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
