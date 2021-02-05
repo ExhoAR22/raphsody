440a4ffedb76ad629336792825087d14804dddb7
ExhoAR22 <exhoar@gmail.com>
2021-02-05 17:09:04 +0200

    Use new types and definitions in uefi

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-use core::ffi::c_void;
+use uefi::types::{Handle, SystemTable, Status};
 
 #[no_mangle]
-pub extern "C" fn efi_main(_image_handle: *const c_void, _system_table: *const c_void) -> u64 {
-    loop {}
+pub extern "C" fn efi_main(image_handle: Handle, system_table: *const SystemTable) -> Status {
+    uefi::utils::init(image_handle, system_table);
+    Status::Success
 }
 
 #[panic_handler]
